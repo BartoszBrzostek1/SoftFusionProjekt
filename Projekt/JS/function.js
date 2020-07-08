1b05b74e4283942b536780f90a19a3d1accdde52
Bartuzy <bartuzi97@gmail.com>
2020-07-08 16:24:17 +0200

    08.07.2020 Zastosowanie SASS, BEM i JS w index

--- Projekt/JS/function.js
+++ Projekt/JS/function.js
@@ -1,8 +1,24 @@
-function bmi() {
-   var a = document.getElementById('waga').value;
-   var b = document.getElementById('wzrost').value;  
 
-   var result = parseFloat(a) / (parseFloat(b) * parseFloat(b))        
-       
-   document.getElementById('result').value = '= ' + result;
+wynikBMI = () => {
+   const a = document.kalBMI.waga.value;
+   const b = document.kalBMI.wzrost.value;  
+
+   if(a > 0 && b > 0)
+   {
+      const wynik =  a/((b/100) * (b/100));
+      if(wynik < 18.5){
+         document.kalBMI.odp.value = "Poniżej normy"
+      }
+      if(wynik > 18.5 && wynik < 25){
+         document.kalBMI.odp.value = "Norma"
+      }
+      if(wynik > 25){
+         document.kalBMI.odp.value = "Powyżej normy"
+      }
+
+      document.kalBMI.bmiwynik.value = wynik.toFixed(2);
+   }
+   else {
+      alert("Podano złą wartość");
+   }  
 }
